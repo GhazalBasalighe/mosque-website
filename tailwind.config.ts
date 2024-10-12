c8981b8e52d64731b9592424fadc8e4e37f1abef
Ghazal Basalighe <ghazalbasaligheh@gmail.com>
2024-10-12 22:59:54 +0330

    feat : make initial project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,13 @@
-import type { Config } from "tailwindcss";
+const flowbite = require("flowbite-react/tailwind");
 
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    // ...
+    flowbite.content(),
   ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
+  plugins: [
+    // ...
+    flowbite.plugin(),
+  ],
 };
-export default config;
