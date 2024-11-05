e6c31b727e3ca08bb8deeaf5266e5aac5622cd32
Ghazal Basalighe <ghazalbasaligheh@gmail.com>
2024-11-05 22:08:49 +0330

    feat : add scroll header styling

--- app/components/layout/Header/Header.tsx
+++ app/components/layout/Header/Header.tsx
@@ -1,20 +1,42 @@
 "use client";
+import { useState, useEffect } from "react";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 
-export default function Component() {
+export default function Header() {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 0);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <header className="bg-teal-800 text-white p-4 pb-10 z-10 shadow-sm sticky top-0">
+    <header
+      className={`p-4 pb-10 z-10 sticky top-0 transition-all duration-300 ${
+        isScrolled ? "bg-teal-600 shadow-md" : "bg-teal-700 shadow-sm"
+      }`}
+    >
       <div className="container mx-auto">
         <div className="flex justify-between items-center">
-          <div className="text-2xl font-bold ">بسم الله الرحمن الرحيم</div>
+          <div className="text-2xl font-bold">بسم الله الرحمن الرحيم</div>
           <Link href="/auth">
             <Button variant="outline" size="lg" className="text-white">
               ورود
             </Button>
           </Link>
         </div>
-        <nav className="mt-4 absolute top-1/2 right-1/2 translate-x-1/2 p-4 bg-teal-600/30 shadow-sm rounded-lg backdrop-blur-xl  z-20 w-1/3">
+        <nav
+          className={`mt-4 absolute top-1/2 right-1/2 translate-x-1/2 p-4 shadow-sm rounded-lg backdrop-blur-xl z-20 w-1/3 ${
+            isScrolled ? "bg-teal-400/50" : "bg-teal-600/30"
+          }`}
+        >
           <ul className="flex space-x-4 justify-center gap-4">
             <li>
               <Link
